142d96e5b21c2dc3d2639a207ccb591949f73266
Gema Handika <ggemahandika88@gmail.com>
2026-06-23 17:14:24 +0700

    update base path github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite' // <--- 1. Import plugin baru di sini
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 🔑 Sudah disesuaikan dengan nama repository GitHub Anda
+  base: '/vue-project/', 
+  
   plugins: [
     vue(),
     vueDevTools(),
-    tailwindcss(), // <--- 2. Daftarkan plugin di sini
+    tailwindcss(),
   ],
   resolve: {
     alias: {
